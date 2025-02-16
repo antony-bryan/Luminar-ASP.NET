3abf1472f2f338225d58da5491f73ded296ee07e
Antony Bryan <antonybryan2.00@gmail.com>
2025-02-16 23:25:59 +0530

    ajax clock

---
+++ ASP.NET/Ajax example/ajax_clock.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ajax_example
+{
+    public partial class ajax_clock : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string time_with_sec = DateTime.Now.ToString();
+            Label1.Text = time_with_sec;
+        }
+    }
+}
